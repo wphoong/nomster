ed64de188d6b8e82a0e2244bf19a37d598119f59
Wilson <wphoong@gmail.com>
2017-04-15 21:38:08 +0000

    restricted edit,update, and delete for places to logged user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
   def index
@@ -23,18 +23,32 @@ class PlacesController < ApplicationController
 
   def edit
       @place = Place.find(params[:id])
+
+      if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+      end
   end
 
   def update
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
-    redirct_to root_path
+    redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
+
   end
 
 
