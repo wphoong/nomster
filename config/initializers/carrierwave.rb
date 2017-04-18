5e67cd37072b781dd9d61bb8c5a1d7b153e88e20
Wilson <wphoong@gmail.com>
2017-04-18 22:24:06 +0000

    added region to carrierwave.rb in initializers folder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],
+    :region => ENV[‘S3_REGION’]                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
