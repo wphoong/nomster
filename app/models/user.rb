2f89d9caf7ea925d08cb3e1f8225024dd04a9590
Wilson <wphoong@gmail.com>
2017-04-20 20:52:03 +0000

    added dependent destroy to place and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 
-  has_many :places
-  has_many :comments
+  has_many :places, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
